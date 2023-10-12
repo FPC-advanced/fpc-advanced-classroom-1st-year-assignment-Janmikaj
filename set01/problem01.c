c1275117bce83164c0f917bbba6a113d389fe1e6
Janmikaj <jayamj.bsc23@rvu.edu.in>
2023-10-12 15:07:03 +0530

    committed 10/12/2023, 3:07:03 PM

--- set01/problem01.c
+++ set01/problem01.c
@@ -1,1 +1,7 @@
 // 1. Write a C program to print your name.
+#include <stdio.h>
+int main()
+{
+    printf("jayam janmika\n");
+    return 0;
+}
--- set01/problem02.c
+++ set01/problem02.c
@@ -1,2 +1,20 @@
 //2. Write a C program to add two numbers.
-
+#include <stdio.h>
+ int add(int a,int b);
+ int main()
+ {
+    int a,b,sum;
+    printf("Enter the value of a\n");
+    scanf("%d", &a);
+    printf("Enter the value of b\n");
+    scanf("%d", &b);
+    sum=add(a,b);
+    printf("sum is %d\n",sum);
+    return 0;
+ }
+ int add(int a,int b)
+ {
+    int sum;
+    sum=a+b;
+    return sum;
+ }
