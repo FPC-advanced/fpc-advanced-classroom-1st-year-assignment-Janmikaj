db4572132027ccdd0f8c741a01c91d54b3646536
Janmikaj <jayamj.bsc23@rvu.edu.in>
2023-10-16 09:32:02 +0530

    committed 10/16/2023, 9:32:02 AM

--- set02/problem01.c
+++ set02/problem01.c
@@ -23,5 +23,5 @@ void find_area(float base , float height, float *area)
 }
 void output(float base, float height, float area)
 {
-    printf("The area of the triangle is %f",area);
+    printf("The area of the triangle is %f\n",area);
 }
