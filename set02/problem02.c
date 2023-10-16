40948185e412fb70b4e2b899a4705b7d996e4cd0
Janmikaj <jayamj.bsc23@rvu.edu.in>
2023-10-16 10:10:54 +0530

    committed 10/16/2023, 10:10:54 AM

--- set02/problem02.c
+++ set02/problem02.c
@@ -5,5 +5,36 @@ int check_scalene(int a, int b, int c);
 void output(int a, int b, int c, int isscalene);
 int main()
 {
-    
+    int side1,side2,side3;
+    side1 = input_side();
+    side2 = input_side();
+    side3 = input_side();
+    int isscalene= check_scalene(side1,side2,side3);
+    output(side1,side2,side3,isscalene);
+    return 0;
+}
+int input_side()
+{
+    int side;
+    printf("Enter the side of triangle\n");
+    scanf("%d",&side);
+    return 0;
+}
+int check_scalene(int a, int b, int c)
+{
+if (a != b && b != c && a != c) {
+        return 1;  
+    } else {
+        return 0; 
+    }
+}
+
+void output(int a, int b, int c, int isScalene)
+{
+
+    if (isScalene) {
+        printf("The triangle is scalene.\n");
+    } else {
+        printf("The triangle is not scalene.\n");
+    }
 }
