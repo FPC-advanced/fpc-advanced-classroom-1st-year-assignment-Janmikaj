6e7c64eeba0358d4faf08c371fdb65d6220f3f74
Janmikaj <jayamj.bsc23@rvu.edu.in>
2023-10-16 10:32:00 +0530

    committed 10/16/2023, 10:32:00 AM

--- set01/problem06.c
+++ set01/problem06.c
@@ -1,42 +1,41 @@
-//Write a C program to compare three numbers using pass by reference
+//6. Write a C program to compare three numbers using *pass by reference*
 #include <stdio.h>
-int input();
-void compare(int a,int b,int c,int *largest);
-void output(int a,int b,int c,int largest);
-
+int input(int *a, int *b, int *c);
+void compare(int a, int b, int c, int *largest);
+void output(int a, int b, int c, int largest);
 int main()
 {
-int a,b,c,largest=0;
-a=input();
-b=input();
-c=input();
-compare(a,b,c,&largest);
-output(a,b,c,largest);
-return 0;
+    int a,b,c,largest;
+    input(&a,&b,&c);
+    compare(a,b,c,&largest);
+    output(a,b,c,largest);
+    return 0;
 }
-int input()
+int input(int *a, int *b, int *c)
 {
- int x;
- printf("enter the value\n");
- scanf("%d",&x);
- return x;
-} 
-void compare(int a,int b,int c,int *largest)
-{
-  if (a>b && a>c)
-  {
-    *largest=a;
-  }
-  if (b>a && b>c)
-  {
-    *largest=b;
-  }
-  else
-  { 
-   *largest=c;
-  }
+    printf("Enter the value of a\n");
+    scanf("%d",a);
+    printf("Enter the value of b\n");
+    scanf("%d",b);
+    printf("Enter the value of c\n");
+    scanf("%d",c);
 }
-void output(int a,int b,int c,int largest)
+void compare(int a, int b, int c, int *largest)
 {
-  printf("the largest of %d",largest);
+    if(a>=b && a>=c)
+    {
+        *largest=a;
+    }
+    if(b>=a && b>=c)
+    {
+        *largest=b;
+    }
+    else
+    {
+        *largest=c;
+    }
+}
+void output(int a, int b, int c, int largest)
+{
+    printf("The largest is %d",largest);
 }
---
+++ set02/problem03.c
@@ -0,0 +1,9 @@
+//Write a program find whether a number is a composite number
+#include <stdio.h>
+int input_number();
+int is_composite(int n);
+void output(int n, int result);
+int main()
+{
+    
+}
