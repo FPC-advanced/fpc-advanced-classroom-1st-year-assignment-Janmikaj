342253d60093501b5319193f90a17cf31eceeb4d
Janmikaj <jayamj.bsc23@rvu.edu.in>
2023-10-16 09:52:08 +0530

    committed 10/16/2023, 9:52:08 AM

--- set02/problem02.c
+++ set02/problem02.c
@@ -1,1 +1,9 @@
 //Write a program to find if a triangle is scalene.
+#include <stdio.h>
+int input_side();
+int check_scalene(int a, int b, int c);
+void output(int a, int b, int c, int isscalene);
+int main()
+{
+    
+}
